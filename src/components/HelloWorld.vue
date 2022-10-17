d685ae7f90843790c5b9e32d370a3b053d46f442
odatalioruo <85120470+odatalioruo@users.noreply.github.com>
2022-10-17 16:37:19 -0400

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,44 +5,53 @@
       depois fazer um for e analizar os valores de todo mundo, depois botar, 
   -->
   <div class="iku"></div>
-    <p class="sexo">Versão 0.00000000000000000000000000000000000000000000000000000000000000000000000000000..............</p>
-    <p>Nome do personagem</p>
-    <input class="vitin" type="text"><br>
+    <p id="sexo">Versão 0.0000000000000000000000000000000000000000000000000000000000000..............</p>
+    <p>insira texto</p>
+    <input id="vitin" type="text"><br>
     <p>Dado</p>
-    <input class="breno" type="number" min='1' max="20"><br><br>
+    <input id="breno" type="number" min='1' max="20"><br><br>
     <button v-on:click="Turno()">adcionar player</button><br><br>
     <!-- um for para cada player é melhor <div id="ordem_player">{{players}}</div> -->
-    <div class="resultado"></div>
+    <p id="resultado">a</p>
   </div>
 </template>
 
 <script>
 export default {
+  
   name: 'HelloWorld',
   props: {
     msg: String
   },
   data(){
       return {
-        players: []
+        players: [],
+        rest: []
       }
   },
   methods:{
     // função pra botar o numero e calcular o player que vai primeiro. Mostra só o nome e o dado
+
     Turno(){
-        var pers = document.querySelector('.vitin')
-        console.log(pers)
-        return pers
+        var resultado = document.querySelector('#resultado')
+        var pers = document.querySelector('#vitin').value;
+        var dado = document.querySelector('#breno').value;
+        this.players.push([pers,dado])
+        console.log(this.players)
+        resultado.innerHTML = '';
+      for(let i = 0;i<this.players.length;i++){
+        resultado.append((i+1)+' - '+this.players[i][0]+'');
+        resultado.append('\n')  
+      }
     }
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 button{
   padding:11px;
   border-radius: .4em;
-  background:#4499aa;
+  background:#7aaabb;
 }
 </style>
